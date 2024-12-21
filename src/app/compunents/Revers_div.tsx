c346ac72e85b4aaa0d4c136f93ae42f6cf4de563
unknown <abdullahrustam12345@gmail.com>
2024-12-21 14:34:55 +0500

    Push Assingment

--- src/app/compunents/Revers_div.tsx
+++ src/app/compunents/Revers_div.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import Button from "@/app/compunents/Button";
 const Simple_div = ( btn_text, p_text, h_text, img,classe ) => {
   return (
