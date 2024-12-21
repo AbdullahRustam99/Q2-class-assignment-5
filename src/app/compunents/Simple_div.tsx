73006ab8a2d9c60f1a50cd18fa9880107fd866ea
unknown <abdullahrustam12345@gmail.com>
2024-12-21 15:56:07 +0500

    Push Assingment

--- src/app/compunents/Simple_div.tsx
+++ src/app/compunents/Simple_div.tsx
@@ -1,27 +1,31 @@
-"use client";
 import React from "react";
-import { StaticImageData } from "next/image";
+import Image,{ StaticImageData } from "next/image";
+
 interface Contenttype {
   p_text?: string;
   h_text?: string;
   classe?: string;
-  img?: StaticImageData;
+  img: StaticImageData;
 }
 
-const Simple_div = ({p_text, h_text, img ,classe}:Contenttype) => {
+const Simple_div = (propes:Contenttype) => {
   return (
     
     <div className="flex justify-center items-center gap-10 w-full max-md:flex-col    ">
       <div className="w-[552px] h-[361px] flex flex-col gap-[35px] max-md:w-full max-md:p-10 max-md:text-center max-md:justify-center max-md:items-center max-sm:w-full max-sm:p-0">
         <h2 className="text-[64px] font-bold leading-[70px] tracking-wide  max-sm:leading-10  max-sm:text-[36px]">
-          {h_text}
+          {propes.h_text}
         </h2>
         <p className="text-[18px] font-normal leading-[30px] max-sm:tracking-[2px]">
-          {p_text}
+          {propes.p_text}
         </p>
         <button>lets go</button>
       </div>
-      <div className={classe}>{img}</div>
+     {propes.img ? (
+             <Image className={propes.classe || ""} src={propes.img} alt="img" />
+           ) : (
+             <div></div>
+           )}
     </div>
   );
 };
